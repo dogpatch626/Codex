4ec990ef70c825c5748638ec815a821354fd32e3
dogpatch626 <djpepin1299@gmail.com>
2023-01-09 15:44:50 -0500

    figuring how to write vigenere out

--- src/vigenere_cipher.rs
+++ src/vigenere_cipher.rs
@@ -1,8 +1,16 @@
 pub fn encrypt(plain_text: &str, key: &str) -> String{
 
     let mut result:String = String::from("");
-    
-    if key.len() == 0{  return plain_text.to_string()}; 
+    let key_len = key.len();
+    if key_len == 0{  return plain_text.to_string()}; 
     if plain_text.len() == 0{ return plain_text.to_string() }
+
+    let filtered_key = key.to_ascii_lowercase(); 
+    let filtered_plain_text = plain_text.to_ascii_lowercase(); 
+
+    filtered_plain_text.chars().map(|c| {
+    
+    });
+
     return result; 
 }
