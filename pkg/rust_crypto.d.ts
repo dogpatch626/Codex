7268d807914b80cf35ba81388e99a680420071ee
dogpatch626 <djpepin1299@gmail.com>
2023-01-23 19:40:30 -0500

    added rust wasm

---
+++ pkg/rust_crypto.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} plain_text
+* @param {number} key
+* @returns {string}
+*/
+export function encrypt(plain_text: string, key: number): string;
+/**
+* @param {string} plain_text
+* @param {number} key
+* @returns {string}
+*/
+export function decrypt(plain_text: string, key: number): string;
